c31d7b6d34ab3405e55d5776808edcd6145bd756
Petr Listov <plistov@Petrs-MacBook-Pro-2.local>
2019-09-30 17:33:32 +0200

    + embedded resource mpc attempt;

--- src/control/double_integrator_model.hpp
+++ src/control/double_integrator_model.hpp
@@ -6,23 +6,24 @@
 template <typename _Scalar = double>
 struct System
 {
-    System(){A << 0, 0, 1, 0; B << 1, 0;}
+    System(){A << 0,0,0, 0,1,0, 0,0,0; B << 1,0,0, 0,0,0, 0,0,-1; c << 0,0,0.25;}
     ~System(){}
 
     using Scalar     = _Scalar;
-    using State      = Eigen::Matrix<Scalar, 2, 1>;
-    using Control    = Eigen::Matrix<Scalar, 2, 1>;
+    using State      = Eigen::Matrix<Scalar, 3, 1>; // x1, x2, e
+    using Control    = Eigen::Matrix<Scalar, 3, 1>; // u, D, v
     using Parameters = Eigen::Matrix<Scalar, 1, 1>;
 
     Eigen::Matrix<Scalar, State::RowsAtCompileTime, State::RowsAtCompileTime> A;
-    Eigen::Matrix<Scalar, State::RowsAtCompileTime, 1> B;
+    Eigen::Matrix<Scalar, State::RowsAtCompileTime, Control::RowsAtCompileTime> B;
+    Eigen::Matrix<Scalar, State::RowsAtCompileTime, 1> c;
 
     /** the one for automatic differentiation */
     template<typename DerivedA, typename DerivedB, typename DerivedC, typename DerivedD>
     void operator() (const Eigen::MatrixBase<DerivedA> &state, const Eigen::MatrixBase<DerivedB> &control,
                      const Eigen::MatrixBase<DerivedC> &param, Eigen::MatrixBase<DerivedD> &value) const
     {
-        value = control[1] * (A * state + B * control[0]);
+        value = control[1] * (A * state + B * control + c);
     }
 };
 
@@ -30,21 +31,21 @@ template<typename _Scalar = double>
 struct Lagrange
 {
     using Scalar = _Scalar;
-    using State = Eigen::Matrix<Scalar, 2, 1>;
-    using Control  = Eigen::Matrix<Scalar, 2, 1>;
+    using State = Eigen::Matrix<Scalar, 3, 1>;
+    using Control  = Eigen::Matrix<Scalar, 3, 1>;
     using Parameters = Eigen::Matrix<Scalar, 1, 1>;
 
     Eigen::Matrix<Scalar, 1, 1> Q;
     Eigen::Matrix<Scalar, Control::RowsAtCompileTime, Control::RowsAtCompileTime> R;
-    Eigen::Matrix<Scalar, 1, 2> C;
-    Eigen::Matrix<Scalar, 1, 1> y_ss;
+    Eigen::Matrix<Scalar, 1, 3> C;
 
     Lagrange(){
         Q << 100;
-        R << 0.5, 0,
-             0, 0;
-        y_ss << 1.0;
-        C << 0, 1;
+        R << 0.5, 0, 0,
+              0,  0, 0,
+              0,  0, 0;
+
+        C << 0, 1, 0;
     }
     ~Lagrange(){}
 
@@ -53,7 +54,8 @@ struct Lagrange
     void operator() (const Eigen::MatrixBase<DerivedA> &state, const Eigen::MatrixBase<DerivedB> &control,
                      const Eigen::MatrixBase<DerivedC> &param, CostT &value) const
     {
-        value = (C * state - y_ss).dot(Q * (C * state - y_ss)) + control.dot(R * control);
+        // param = y_ss
+        value = (C * state - param).dot(Q * (C * state - param)) + control.dot(R * control) - 1e-3 * state[2];
     }
 
 };
@@ -64,24 +66,24 @@ struct Mayer
     Mayer(){
         Q << 100;
         y_ss << 1.0;
-        C << 0, 1;
+        C << 0, 1, 0;
     }
     ~Mayer(){}
 
     using Scalar = _Scalar;
-    using State = Eigen::Matrix<Scalar, 2, 1>;
-    using Control  = Eigen::Matrix<Scalar, 2, 1>;
+    using State = Eigen::Matrix<Scalar, 3, 1>;
+    using Control  = Eigen::Matrix<Scalar, 3, 1>;
     using Parameters = Eigen::Matrix<Scalar, 1, 1>;
 
     Eigen::Matrix<Scalar, 1, 1> Q;
-    Eigen::Matrix<Scalar, 1, 2> C;
+    Eigen::Matrix<Scalar, 1, 3> C;
     Eigen::Matrix<Scalar, 1, 1> y_ss;
 
     template<typename StateT, typename CostT>
     void operator() (const Eigen::MatrixBase<StateT> &state, CostT &value) const
     {
         using ScalarT = typename Eigen::MatrixBase<StateT>::Scalar;
-        value = (C * state - y_ss).dot(Q.template cast<ScalarT> * (C * state - y_ss));
+        value = (C * state - y_ss).dot(Q.template cast<ScalarT>() * (C * state - y_ss));
     }
 };
 
--- src/control/resource_mpc.cpp
+++ src/control/resource_mpc.cpp
@@ -8,7 +8,131 @@
 #include "solvers/sqp.hpp"
 
 
+template <typename Solver>
+void callback(void *solver_p)
+{
+    Solver& s = *static_cast<Solver*>(solver_p);
+    std::cout << s._x.transpose() << std::endl;
+}
+
+using Problem = polympc::OCProblem<System<double>, Lagrange<double>, Mayer<double>>;
+using Approximation = Chebyshev<3, GAUSS_LOBATTO, double>; // POLY_ORDER = 3
+
+using controller_t = polympc::nmpc<Problem, Approximation, sqp::SQP>;
+using var_t = controller_t::var_t;
+using dual_t = controller_t::dual_t;
+using State = controller_t::State;
+using Control = controller_t::Control;
+using Parameters = controller_t::Parameters;
+
+enum
+{
+    NX = State::RowsAtCompileTime,
+    NU = Control::RowsAtCompileTime,
+    NP = Parameters::RowsAtCompileTime,
+    VARX_SIZE = controller_t::VARX_SIZE,
+    VARU_SIZE = controller_t::VARU_SIZE,
+    VARP_SIZE = controller_t::VARP_SIZE,
+    VAR_SIZE = var_t::RowsAtCompileTime,
+};
+
+void print_info(void)
+{
+    std::cout << "NX = " << NX << "\n";
+    std::cout << "NU = " << NU << "\n";
+    std::cout << "NP = " << NP << "\n";
+    std::cout << "VARX_SIZE = " << VARX_SIZE << "\n";
+    std::cout << "VARU_SIZE = " << VARU_SIZE << "\n";
+    std::cout << "VARP_SIZE = " << VARP_SIZE << "\n";
+    std::cout << "VAR_SIZE = " <<  VAR_SIZE << "\n";
+
+    std::cout << "controller_t size: " << sizeof(controller_t) << "\n";
+    std::cout << "controller_t::cost_colloc_t size: " << sizeof(controller_t::cost_colloc_t) << "\n";
+    std::cout << "controller_t::ode_colloc_t size: " << sizeof(controller_t::ode_colloc_t) << "\n";
+    std::cout << "controller_t::sqp_t size: " << sizeof(controller_t::SolverImpl) << "\n";
+    std::cout << "controller_t::sqp_t::qp_t size: " << sizeof(controller_t::SolverImpl::qp_t) << "\n";
+    std::cout << "controller_t::sqp_t::qp_solver_t size: " << sizeof(controller_t::SolverImpl::qp_solver_t) << "\n";
+}
+
+void print_duals(const dual_t& y)
+{
+    Eigen::IOFormat fmt(3, 0, ", ", ",", "[", "]");
+    std::cout << "duals" << std::endl;
+    std::cout << "ode   " << y.template segment<VARX_SIZE>(0).transpose().format(fmt) << std::endl;
+    std::cout << "x     " << y.template segment<VARX_SIZE-NX>(VARX_SIZE).transpose().format(fmt) << std::endl;
+    std::cout << "x0    " << y.template segment<NX>(2*VARX_SIZE-NX).transpose().format(fmt) << std::endl;
+    std::cout << "u     " << y.template segment<VARU_SIZE>(2*VARX_SIZE).transpose().format(fmt) << std::endl;
+}
+
+void print_sol(const var_t& sol)
+{
+    Eigen::IOFormat fmt(4, 0, ", ", ",", "[", "]");
+    std::cout << "xyt" << std::endl;
+    for (int i = 0; i < VARX_SIZE/NX; i++) {
+       std::cout << sol.segment<NX>(NX*i).transpose().format(fmt) << ",\n";
+    }
+    std::cout << "u" << std::endl;
+    for (int i = 0; i < VARU_SIZE/NU; i++) {
+       std::cout << sol.segment<NU>(VARX_SIZE+NU*i).transpose().format(fmt) << ",\n";
+    }
+}
+
+template <typename Var>
+void save_csv(const char *name, const std::vector<Var>& vec)
+{
+    std::ofstream out(name);
+    Eigen::IOFormat fmt(Eigen::FullPrecision, Eigen::DontAlignCols, ",", ",", "", "");
+    for (const Var& x : vec) {
+        out << x.transpose().format(fmt) << "\n";
+    }
+}
+
+template<typename Derived>
+inline bool is_nan(const Eigen::MatrixBase<Derived>& x)
+{
+    return !((x.array() == x.array())).all();
+}
+
+
+
 int main(void)
 {
+    controller_t robot_controller;
+    // robot_controller.m_solver.settings().iteration_callback = callback<controller_t::sqp_t>;
+
+    State x = {0, 0, 0.5};
+    State dx;
+    Control u;
+    Parameters p(1.0); //ref point
+
+    // bounds
+    controller_t::State xu, xl;
+    xu << -100, -5, 0.0;
+    xl <<  100,  5, 1.0;
+    controller_t::Control uu, ul;
+    uu << -10,  0.5, 0.25;
+    ul <<  10,  2.0, 0.5;
+
+    robot_controller.setStateBounds(xl, xu);
+    robot_controller.setControlBounds(ul, uu);
+    robot_controller.setParameters(p);
+    robot_controller.disableWarmStart();
+
+    print_info();
+    std::cout << "x0: " << x.transpose() << std::endl;
+
+    robot_controller.computeControl(x);
+    robot_controller.getOptimalControl(u);
+
+    var_t sol;
+    robot_controller.getSolution(sol);
+
+    std::cout << "Solution: " << sol << "\n";
+
+    std::cout << "iter " << robot_controller.m_solver.info().iter << "  ";
+    std::cout << "qp " << robot_controller.m_solver.info().qp_solver_iter << "  ";
+    std::cout << "x " << x.transpose() << "    ";
+    std::cout << "u " << u.transpose() << std::endl;
+
     return 0;
 }
